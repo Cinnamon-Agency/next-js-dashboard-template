e506188952997c24aaa591a28a4a73f3f3f831b7
Martina Večerin <martina.vecerin@cinnamon.agency>
2025-04-17 15:13:58 +0200

    fix config

--- next.config.js
+++ next.config.js
@@ -21,10 +21,6 @@ const nextConfig = {
 	// experimental: {
 	// 	appDir: true
 	// },
-	i18n: {
-		locales: ['en'],
-		defaultLocale: 'en'
-	},
 	webpack(config) {
 		config.module.rules.push({
 			test: /\.svg$/,
