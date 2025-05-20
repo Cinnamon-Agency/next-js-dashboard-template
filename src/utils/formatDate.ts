15cbe72fa6d8b6b031706df3e1afa75534aa00bb
Martina Večerin <martina.vecerin@cinnamon.agency>
2025-05-20 15:44:25 +0200

    change date format

--- src/utils/formatDate.ts
+++ src/utils/formatDate.ts
@@ -2,7 +2,7 @@ import { format, parseISO } from 'date-fns'
 
 export const formatDate = (date: string) => {
 	try {
-		return format(parseISO(date), 'MMM d yyyy, h:mma')
+		return format(parseISO(date), 'dd.MM.yyyy, hh:mm')
 	} catch (error) {
 		return 'Invalid date'
 	}
